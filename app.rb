d380291b4868f0739b43a99faf8ec37133f6f34a
Paul Kuruvilla <rohitpaulk@gmail.com>
2017-05-07 21:12:18 +0530

    Add root page

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/api/v1/unfurl.json' do
   '{"status": 200}'
 end
+
+get '/' do
+  <<-HTML
+    <h2 style="text-align:center;margin-top:40px;">Unfurld is live!</h2>
+
+    <br/></br>
+
+    <p style="text-align:center;">
+      Hit <a href="/api/v1/unfurl.json">/api/v1/unfurl.json</a> to get started.
+    </p>
+  HTML
+end
